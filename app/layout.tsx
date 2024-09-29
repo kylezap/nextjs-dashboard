03edcd645f2465186492b7c9f49d2e2e776b8d48
kylezap <kzapcic+github@gmail.com>
2024-09-29 14:06:50 -0400

    changes to routes and layout for dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>{" "}
-    </html>
+    <html lang="en"><body className={`${inter.className} antialiased`}>{children}</body>{" "}</html>
   );
 }
